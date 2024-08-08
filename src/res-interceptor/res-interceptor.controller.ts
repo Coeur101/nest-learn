22684e1659489f73eb8de1d69326e912b488ac32
Coeurxx <2543046796@qq.com>
2024-08-08 20:38:07 +0800

    feat: post body validator

--- src/res-interceptor/res-interceptor.controller.ts
+++ src/res-interceptor/res-interceptor.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Next, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, HttpStatus, Next, Post, UseInterceptors, ValidationPipe } from '@nestjs/common';
 import { ResInterceptorService } from './res-interceptor.service';
 import { ApiInterceptor } from 'src/common/api.interceptor';
 import { TimeOut } from 'src/common/timeout';
+import { resDtos } from 'src/dtos/res';
 
 @Controller('res-interceptor')
 @UseInterceptors(ApiInterceptor)
@@ -19,5 +20,16 @@ export class ResInterceptorController {
   async getTimeOut() {
     await new Promise(resolve => setTimeout(() => resolve, 6000))
     return 'hello'
+  }
+  @Post('aaa')
+  // 使用参数校验
+  // 也可以使用自定义的参数报错提示
+  dataValidator(@Body(new ValidationPipe({
+    // 自定义的报错行为
+    // exceptionFactory: (msg) => {
+    //   throw new HttpException('参数校验错误', HttpStatus.NOT_FOUND)
+    // }
+  })) obj: resDtos) {
+    return obj
   }
 }
